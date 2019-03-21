32dd6ee4c6ec8bf4567fce808a31b7e147f4aeb2
jagadeeshreddy.bj@gmail.com <Jaya@0123>
2019-03-21 13:52:02 +0530

    Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PgComponent } from './pg/pg.component';
 
-const routes: Routes = [];
+const routes:Routes=[
+  {path: '', redirectTo:'/Pg', pathMatch:'full'},
+  {path:'***', component: HomeComponent},
+  
+  {path:'Home', component:HomeComponent},
+  {path:'Pg', component:PgComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  
+ }
